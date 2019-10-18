e2a7289f673eb0656d4fbb2b2f07e49a8c06324a
antoria <antoiria91@gmail.com>
2019-10-18 15:26:19 -0400

    Create ClassRegistry.hpp

---
+++ main/include/ClassRegistry.hpp
@@ -0,0 +1,36 @@
+#include "game_object.hpp"
+#include <map>
+#include <functional>
+
+class ClassRegistry
+{
+public:
+
+	static ClassRegistry& Get()
+	{
+		static ClassRegistry instance;
+		return instance;
+	}
+
+	template<class T>
+	void RegisterClass()
+	{
+		assert(classCreators.find(T::mClassID) == classCreators.end());
+		classCreators[T::mClassID] = T::CreateInstance;
+	}
+
+	GameObject Create(uint32_t classId)
+	{
+		GameObject go;
+
+		std::function<GameObject()> creator = classCreators[classId];
+		go = creator();
+		
+		return go;
+	}
+
+
+private:
+	std::map<uint32_t, std::function<GameObject()>> classCreators;
+
+};
